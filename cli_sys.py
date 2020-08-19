3778631a58d0036d4bb36b4100018559d933d995
Philippe Huot <philippe.ht@gmail.com>
2020-08-18 23:47:28 -0400

    initialize repo

---
+++ cli_sys.py
@@ -0,0 +1,18 @@
+import sys
+
+from getpass import getpass
+name = input("What's your name?\n")
+pw = getpass("What's your password?\n")
+
+# print(name, pw)
+
+if __name__ == '__main__':
+    try:
+        name = sys.argv[1]
+    except:
+        name = input("What's your name?\n")
+    from getpass import getpass
+    name = input("What's your name?\n")
+    pw = getpass("What's your password?\n")
+
+    
